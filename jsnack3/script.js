7305a382383d062786d3cb2fbbd1858640cbe939
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-07 15:01:16 +0200

    snack3-release

---
+++ jsnack3/script.js
@@ -0,0 +1,13 @@
+const userNumber = parseInt(prompt("Inseri qui un il tuo numero fortunato"))
+
+for(let i= 0; i < userNumber; i++){
+    const numberList = []
+    for( let y = 0; y < 10; y++){
+        const randomNumber = (Math.floor(Math.random() * 100) +1);
+        numberList.push(randomNumber);
+    }
+    console.log(numberList)   
+}
+
+
+
