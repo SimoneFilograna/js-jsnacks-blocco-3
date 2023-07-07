15e3cc6f7026cdedea858f4ef5264878256d09be
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-07 13:58:44 +0200

    fixed-color

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,9 +6,9 @@ for(let i = 0; i < 20; i++){
     const randomNumber = Math.floor(Math.random() * 200);
     numList.push(randomNumber);
     if( randomNumber % 2 === 0){
-        redDiv.innerHTML += (`<p>${randomNumber}</p>`)
-    } else {
         greenDiv.innerHTML += (`<p>${randomNumber}</p>`)
+    } else {
+        redDiv.innerHTML += (`<p>${randomNumber}</p>`)
     }
 }
 
