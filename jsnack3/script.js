7f6fbaa8435256ac859e65efad8e64347d90c88a
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-07 15:07:21 +0200

    fixed-snack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,11 +3,8 @@ const userNumber = parseInt(prompt("Inseri qui un il tuo numero fortunato"))
 for(let i= 0; i < userNumber; i++){
     const numberList = []
     for( let y = 0; y < 10; y++){
-        const randomNumber = (Math.floor(Math.random() * 100) +1);
+        const randomNumber = (Math.floor(Math.random() * 100) + 1);
         numberList.push(randomNumber);
     }
     console.log(numberList)   
 }
-
-
-
